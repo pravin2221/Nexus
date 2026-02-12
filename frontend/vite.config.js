9e1dd964add39083a830310a3f093e63ce762a8a
Pravith1 <pravith1489181@gmail.com>
2026-02-12 19:07:24 +0530

    update base path in vite.config.js to root

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/nexus/', // App runs at /nexus path
+  base: '/', // App runs at /nexus path
   plugins: [react()],
 })
